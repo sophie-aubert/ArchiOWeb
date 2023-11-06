d6b6828bc6eee88c9d5009e36693f8e9ff605d8b
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-11-06 15:34:58 +0100

    Test Render sans DB

--- Application/app.js
+++ Application/app.js
@@ -1,31 +1,31 @@
 import express from "express";
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
-// Connexion à MongoDB avec Mongoose
-mongoose.connect(
-  "mongodb+srv://admin:TheNiche1234@clustertheniche.dzl3a3c.mongodb.net/AppTheNiche?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// // Connexion à MongoDB avec Mongoose
+// mongoose.connect(
+//   "mongodb+srv://admin:TheNiche1234@clustertheniche.dzl3a3c.mongodb.net/AppTheNiche?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
-// Gestion de la connexion réussie
-mongoose.connection.on("connected", () => {
-  console.log("Connexion à MongoDB établie avec succès");
-});
+// // Gestion de la connexion réussie
+// mongoose.connection.on("connected", () => {
+//   console.log("Connexion à MongoDB établie avec succès");
+// });
 
-// Gestion des erreurs de connexion
-mongoose.connection.on("error", (err) => {
-  console.error("Erreur de connexion à MongoDB : " + err);
-});
+// // Gestion des erreurs de connexion
+// mongoose.connection.on("error", (err) => {
+//   console.error("Erreur de connexion à MongoDB : " + err);
+// });
 
-// Gestion des déconnexions
-mongoose.connection.on("disconnected", () => {
-  console.log("La connexion à MongoDB a été interrompue");
-});
+// // Gestion des déconnexions
+// mongoose.connection.on("disconnected", () => {
+//   console.log("La connexion à MongoDB a été interrompue");
+// });
 
 app.use(express.json());
 
