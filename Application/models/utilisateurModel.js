50be1b3cab91e46164180c4c1624d824e9375cc0
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-11-14 10:49:42 +0100

    Insertion utilisateur final

--- Application/models/utilisateurModel.js
+++ Application/models/utilisateurModel.js
@@ -1,18 +1,24 @@
-const utilisateurSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+import mongoose from "mongoose";
+
+const utilisateurSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  // Autres propriétés...
-});
+  { timestamps: true }
+);
 
 const Utilisateur = mongoose.model("Utilisateur", utilisateurSchema);
 
