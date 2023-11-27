066ec498c48079e7454202abab826d01db34dd0a
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-11-27 18:35:02 +0100

    Ajout de l'accès ADMIN pour les tests

--- Application/tests/create.test.js
+++ Application/tests/create.test.js
@@ -4,14 +4,25 @@ import { expect } from "chai";
 
 describe("Create Operation Tests", () => {
   it("should create a new object", async () => {
-    const response = await supertest(app).post("/annonces").send({
-      titre: "Annonce TEST",
-      description: "Cette annonce à été créée pour les tests",
-      utilisateur: "654cdf45e16f0cbe281f8652",
-      categorie: "Vestes",
-      latitude: 12.34,
-      longitude: 56.78,
+    // Authentification en tant qu'admin
+    const authResponse = await supertest(app).post("/auth/login").send({
+      email: "TheAdmin@admin.com",
+      password: "admin",
     });
+    const authToken = authResponse.body.token;
+
+    // Envoi de la requête de création avec le token d'authentification
+    const response = await supertest(app)
+      .post("/annonces")
+      .set("Authorization", `${authToken}`)
+      .send({
+        titre: "Annonce TEST",
+        description: "Cette annonce a été créée pour les tests",
+        prix: 99.99,
+        categorie: "Vestes",
+        latitude: 12.34,
+        longitude: 56.78,
+      });
 
     console.log(response.body);
     expect(response.status).to.equal(201);
--- Application/tests/update.test.js
+++ Application/tests/update.test.js
@@ -4,15 +4,22 @@ import { expect } from "chai";
 
 describe("Update Operation Tests", () => {
   it("should update an existing object", async () => {
+    // Authentification en tant qu'admin
     const authResponse = await supertest(app).post("/auth/login").send({
       email: "TheAdmin@admin.com",
       password: "admin",
     });
     const authToken = authResponse.body.token;
-    // console.log(authToken);
 
+    // Récupération d'une annonce au hasard depuis la base de données
+    const annoncesResponse = await supertest(app).get("/annonces");
+    const annonces = annoncesResponse.body;
+    const annonceAleatoire =
+      annonces[Math.floor(Math.random() * annonces.length)];
+
+    // Envoi de la requête de mise à jour avec le token d'authentification
     const response = await supertest(app)
-      .put("/annonces/65649339254870eaac0e2150")
+      .put(`/annonces/${annonceAleatoire._id}`)
       .set("Authorization", `${authToken}`)
       .send({
         titre: "Annonce modifiée automatiquement par les tests",
